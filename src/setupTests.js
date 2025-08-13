aae282c393aa455c6473d8a26bcbc090c88a3539
Igor Ricarte <pro-github-ir@zyrog.com>
2025-08-13 17:51:24 +0200

    First tests. Doesnt work yet tho

---
+++ src/setupTests.js
@@ -0,0 +1,8 @@
+import { expect, afterEach } from 'vitest';
+
+// Add any global test setup here
+// For now, we'll keep it simple without testing-library dependencies
+
+afterEach(() => {
+  // Cleanup after each test
+});
